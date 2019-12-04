42203e2eff4470d9b2591f4098af592b6cb8aec1
Kelly Becker <kelly.lynn.becker@gmail.com>
2019-12-04 03:34:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   penguins=["rockhopper","emporor","king","chinstrap"]
-  penguins.push("arrays")
+  penguins.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
