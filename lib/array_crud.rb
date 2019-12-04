4d0e1d850bc7edbb1173252673d0281e5a4f0e41
Kelly Becker <kelly.lynn.becker@gmail.com>
2019-12-04 03:23:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  penguins=["rockhopper","emporor","king","chinstrap"]
   rockhopper = penguins.shift 
 end
 
