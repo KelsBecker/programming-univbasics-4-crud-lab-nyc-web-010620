cb5221e6ef59e02ce58f45572aa4fa94f40614ff
Kelly Becker <kelly.lynn.becker@gmail.com>
2019-12-04 03:43:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   array=["I","am","really","learning","Arrays!"]
-  array=penguins.pop
+  =penguins.pop
 end
 
 def remove_element_from_start_of_array(array)
