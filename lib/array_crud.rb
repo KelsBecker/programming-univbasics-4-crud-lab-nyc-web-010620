b7ec0dc5607e0c9088cd6e342d8f1467d39f2350
Kelly Becker <kelly.lynn.becker@gmail.com>
2019-12-04 03:31:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -32,7 +32,8 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  
+  penguins=["rockhopper","emporor","king","chinstrap"]
+  penguins[0]
 end
 
 def retrieve_last_element_from_array(array)
