9288a3909355f69fd93eaeff4d9ccfb5f6ef6d7d
Kelly Becker <kelly.lynn.becker@gmail.com>
2019-12-04 03:22:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,11 +12,12 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  penguins=["rockhopper","emporor","king","chinstrap"]
   penguins.unshift ("fairy")
 end
 
 def remove_element_from_end_of_array(array)
+  
   chinstrap = penguins.pop
 end
 
