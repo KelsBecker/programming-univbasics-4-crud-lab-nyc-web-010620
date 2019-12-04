9ca23e48591811f01020171a0a17d053a088fa78
Kelly Becker <kelly.lynn.becker@gmail.com>
2019-12-04 03:35:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   penguins=["rockhopper","emporor","king","chinstrap"]
-  penguins.unshift ("fairy")
+  penguins.unshift ("wow")
 end
 
 def remove_element_from_end_of_array(array)
